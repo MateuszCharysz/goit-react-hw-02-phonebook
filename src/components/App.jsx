6d4d4fbedce39e8951d2c1890787e212ff3c12fc
MateuszCharysz <charyszek@interia.pl>
2023-05-28 22:32:06 +0200

    błądzenie z refaktorem

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 // import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import Form from './form/form';
+import { Form } from './form/form';
 import Input from './input/input';
-import Button from './button/button';
+// import Button from './button/button';
 import ContactList from './contact-list/contact-list';
 import css from './App.module.css';
 import { nanoid } from 'nanoid';
@@ -16,8 +16,6 @@ export class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
 
   changeHandler = e => {
@@ -34,17 +32,22 @@ export class App extends Component {
 
   isName = (contacts = this.state.contacts) =>
     contacts.name.toLowerCase() === this.filter.toLowerCase();
-
-  submitHandler = e => {
-    e.preventDefault();
+  submitForm = callback => {
     this.setState(prevState => ({
-      contacts: [
-        ...prevState.contacts,
-        { id: this.idCreate, name: this.state.name, number: this.state.number },
-      ],
+      contacts: [...prevState.contacts, { id: this.idCreate, callback }],
     }));
-    this.formReset();
+    callback();
   };
+  // submitHandler = e => {
+  //   // e.preventDefault();
+  //   this.setState(prevState => ({
+  //     contacts: [
+  //       ...prevState.contacts,
+  //       { id: this.idCreate, name: this.state.name, number: this.state.number },
+  //     ],
+  //   }));
+  //   this.formReset();
+  // };
 
   idCreate = nanoid();
 
@@ -56,34 +59,7 @@ export class App extends Component {
     return (
       <div className={css.container}>
         <h1>Phonebook</h1>
-        <Form
-          render={
-            <>
-              <Input
-                label="Name"
-                type="text"
-                dataName="name"
-                validation="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                required
-                funcChange={this.changeHandler}
-                stateField={this.state.name}
-              />
-              <Input
-                label="Number"
-                type="tel"
-                dataName="number"
-                validation="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                required
-                funcChange={this.changeHandler}
-                stateField={this.state.number}
-              />
-              <Button label="Add contact" typeOfBtn="submit" />
-            </>
-          }
-          funcSubmit={this.submitHandler}
-        />
+        <Form handler={this.submitForm} />
         <h2>Contacts</h2>
         <Input
           label="Find contacts by name"
@@ -94,7 +70,7 @@ export class App extends Component {
           funcChange={this.changeHandler}
           stateField={this.state.filter}
         />
-        <ContactList arr={this.filterContacts()} />
+        <ContactList arr={this.state.contacts} />
       </div>
     );
   }
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,14 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import css from './Form.module.css'
+import css from './Form.module.css';
+import Input from 'components/input/input';
+import Button from 'components/button/button';
+import { nanoid } from 'nanoid';
 
-const Form = ({ render, funcSubmit }) => {
-  return <form onSubmit={funcSubmit} className={css.form}>{render}</form>;
-};
+// const Form = ({ render, funcSubmit }) => {
+//   return <form onSubmit={funcSubmit} className={css.form}>{render}</form>;
+// };
+
+export class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  // funcSubmit = (handler) => {
+  //   callback();
+  // };
+  submitHandler = e => {
+    e.preventDefault();
+    return this.state;
+    // this.formReset();
+  };
+  formReset = () => {
+    this.setState({ name: '', number: '' });
+  };
+  idCreate = nanoid();
+  changeHandler = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  render() {
+    return (
+      <form
+        onSubmit={() => this.props.handler(this.submitHandler)}
+        className={css.form}
+      >
+        <Input
+          label="Name"
+          type="text"
+          dataName="name"
+          validation="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          funcChange={this.changeHandler}
+          stateField={this.state.name}
+        />
+        <Input
+          label="Number"
+          type="tel"
+          dataName="number"
+          validation="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          funcChange={this.changeHandler}
+          stateField={this.state.number}
+        />
+        <Button label="Add contact" typeOfBtn="submit" />
+      </form>
+    );
+  }
+}
+
+export default Form;
 
 Form.propTypes = {
   render: PropTypes.oneOfType([PropTypes.func, PropTypes.element]).isRequired,
   funcSubmit: PropTypes.func.isRequired,
 };
-
-export default Form;
